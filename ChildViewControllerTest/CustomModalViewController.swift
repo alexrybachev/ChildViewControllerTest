3987ffd2bdf5b267ee97c64ec38dbafd45742646
Alex Rybachev <rybachev.as@gmail.com>
2022-05-12 20:55:43 +0400

    добавил CustomModalViewController

--- ChildViewControllerTest/CustomModalViewController.swift
+++ ChildViewControllerTest/CustomModalViewController.swift
@@ -9,5 +9,66 @@ import UIKit
 
 class CustomModalViewController: UIViewController {
     
+    // MARK: - UI Elements
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 16
+        view.clipsToBounds = true
+        return view
+    }()
     
+    private lazy var dimmedView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = maxDimmedAlpha
+        return view
+    }()
+    
+    // MARK: - Private Properties
+    private let maxDimmedAlpha: CGFloat = 0.6
+    private let defaultHeight: CGFloat = 300
+    
+    private var containerViewHeightConstraint: NSLayoutConstraint?
+    private var containerViewBottomConstraint: NSLayoutConstraint?
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupConstraints()
+    }
+    
+    // MARK: = Setup UI Elements
+    private func setupView() {
+        view.backgroundColor = .clear
+    }
+    
+    private func setupConstraints() {
+        view.addSubview(dimmedView)
+        view.addSubview(containerView)
+        dimmedView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+        
+            dimmedView.topAnchor.constraint(equalTo: view.topAnchor),
+            dimmedView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            dimmedView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            dimmedView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+        
+        // Set container to default height
+        containerViewHeightConstraint = containerView.heightAnchor.constraint(equalToConstant: defaultHeight)
+        
+        // Set bottom constant to 0
+        containerViewBottomConstraint = containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        
+        // Activate constraints
+        containerViewHeightConstraint?.isActive = true
+        containerViewBottomConstraint?.isActive = true
+    }
 }
